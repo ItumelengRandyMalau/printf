414e30b235dca912f79cee5f72583b6b2233d270
Just Vishy <109430829+Just-Vishy@users.noreply.github.com>
2023-09-19 16:27:32 +0200

    providing the based file

---
+++ selector.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "handler.c"
+
+int (*selector(char str))(va_list arg)
+{ 
+    if (str == 'c')
+    {
+        return &c_handler;
+    }
+    else if (str == 'd')
+    {
+        return &d_handler; 
+    }
+    else if (str == 's')
+    {
+        return &s_handler;
+    }
+    return 0;
+}
